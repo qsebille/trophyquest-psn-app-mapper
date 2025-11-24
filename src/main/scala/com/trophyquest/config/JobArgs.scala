2ff0a690a327601b42b02e9baf6d1ea7d96eff00
qsebille <quentin.sebille@gmail.com>
2025-11-24 15:43:18 +0100

    Require jobName argument with clearer usage

--- src/main/scala/com/trophyquest/config/JobArgs.scala
+++ src/main/scala/com/trophyquest/config/JobArgs.scala
@@ -4,7 +4,11 @@ import org.rogach.scallop._
 
 class JobArgs(args: Seq[String]) extends ScallopConf(args) {
 
-  val jobName: ScallopOption[String] = opt[String](name = "jobName")
+  val jobName: ScallopOption[String] = opt[String](
+    name = "jobName",
+    required = true,
+    descr = "Required job identifier to run (usage: --jobName <name>)."
+  )
 
   verify()
 }
